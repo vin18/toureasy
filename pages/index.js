d916817bd373a6e7d8a6573c38206e54bb42486d
Vinit Raut <vinit.coder@gmail.com>
2022-03-18 00:49:54 +0530

    Install tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold text-green-500 underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
